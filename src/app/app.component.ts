ccd8eee46d81a995dacf95972ee7a7e03ad9065c
Juwana <juwanazerman@gmail.com>
2019-01-03 22:22:23 -0500

    Create git search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,6 @@ export class AppComponent implements OnInit {
 
   }
   ngOnInit() {
-    this.GitSearchService.gitSearch('angular').then((response) => {
-      alert('Total Libraries Found:' + response.total_count);
-    }, (error) => {
-      alert('Error: ' + error.statusText);
-    });
   }
   // tslint:disable-next-line:member-ordering
   title = 'app is functional!';
