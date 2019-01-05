6e600efec23821e23b611d73a0b8f88d9aa6bd22
Juwana <juwanazerman@gmail.com>
2019-01-04 20:21:13 -0500

    Update new components

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GitSearchService } from '../git-search.service';
 import { GitSearch } from '../git-search';
-
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 @Component({
   selector: 'app-git-search',
   templateUrl: './git-search.component.html',
@@ -10,22 +10,33 @@ import { GitSearch } from '../git-search';
 export class GitSearchComponent implements OnInit {
   searchResults: GitSearch;
   searchQuery: string;
-  constructor(private GitSearchService: GitSearchService) { }
+  displayQuery: string;
+  title: string;
+  _router: any;
+  constructor(private GitSearchService: GitSearchService, private route: ActivatedRoute, private router: Router ) { }
 
   ngOnInit() {
-    this.GitSearchService.gitSearch('angular').then((response) => {
+    this.route.paramMap.subscribe( (params: ParamMap) => {
+      this.searchQuery = params.get('query');
+      this.displayQuery = params.get('query');
+      this.gitSearch();
+    });
+    this.route.data.subscribe( (result) => {
+      this.title = result.title;
+    });
+  }
+
+  gitSearch = () => {
+    this.GitSearchService.gitSearch(this.searchQuery).then( (response) => {
       this.searchResults = response;
     }, (error) => {
       alert('Error: ' + error.statusText);
     });
-}
+  }
 
-gitSearch = () => {
-  this.GitSearchService.gitSearch('this.searchQuery').then((response) => {
-    this.searchResults = response;
-  }, (error) => {
-    alert('Error: ' + error.statusText);
-    });
+  sendQuery = () => {
+    this.searchResults = null;
+    this.router.navigate(['/search/' + this.searchQuery]);
   }
 
 }
